12c8d1b3130124ebf111397d6fdb2fce6a8ebec9
talha-amin <talha.amin@cubixlabs.com>
2023-08-30 20:49:51 +0500

    steps App

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default function App() {
+  return (
+    <div>
+      <Steps />
+    </div>
+  );
+}
+
+function Steps() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handlePrevious() {
+    if (step > 1) setStep((s) => s - 1);
+  }
+
+  function handleNext() {
+    if (step < 3) {
+      setStep((s) => s + 1);
+    }
+  }
+
+  
+
+  return (
+    <div>
+      <button className="close" onClick={() => setIsOpen((is) => !is)}>
+        &times;
+      </button>
+
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+
+          <StepMessage step={step}>{messages[step-1]}
+          </StepMessage>
+
+          <div className="buttons">
+            <Button style ={{backgroundColor: "#7950f2" , color: "#fff"}} onClick={handlePrevious}>
+              <span>🖕</span> Previous </Button>
+
+            <Button style ={{backgroundColor: "#7950f2" , color: "#fff"}}  onClick={handleNext}>
+              Next <span>🖕</span>
+              </Button>
+
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+function StepMessage({step,children}){
+  return (
+  <div className="message">
+    <h3>Step {step}</h3>
+  {children}
+</div>
+  )
+}
+
+function Button ({textcolor,bgColor,onClick,children}){
+  return( 
+  <button style={{backgroundColor: bgColor, color:textcolor}} onClick={onClick}>
+   {children}
+  </button>
+)
+}
+
